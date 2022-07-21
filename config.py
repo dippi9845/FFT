1bdc0c8f396ae1e6f960973ed0084a7f801fa221
Filippo Di Pietro <filippodp9845@gmail.com>
2022-07-21 18:10:09 +0200

    added hostname port and timeout to config

--- config.py
+++ config.py
@@ -1,13 +1,20 @@
-from abc import ABC
 from dataclasses import dataclass
 
 @dataclass
-class Config(ABC):
+class Config:
+    
+    HOSTNAME = "localhost"
+    PORT = 10001
     
     CLIENT_CONFIG = {
+        "hostname" : HOSTNAME,
+        "port" : PORT,
+        "timeout" : 2
 
     }
 
     SERVER_CONFIG = {
-
+        "hostname" : HOSTNAME,
+        "port" : PORT,
+        "timeout" : 2
     }
