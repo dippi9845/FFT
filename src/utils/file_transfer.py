9640a413a35805ea3107dd83bb61158009e3566d
Filippo Di Pietro <filippodp9845@gmail.com>
2022-07-26 16:12:47 +0200

    Update file_transfer.py
    FileData redefined, the old one will be called packet

--- src/utils/file_transfer.py
+++ src/utils/file_transfer.py
@@ -1,8 +1,9 @@
 from hashlib import md5
 from json import dumps
 from typing_extensions import Self
+from utils.config import Config
 
-class DataFile:
+class Packet:
     def __init__(self, data : bytes) -> None:
         self.data = data
         self.hash = md5(data).hexdigest()
@@ -21,11 +22,40 @@ class DataFile:
         else:
             raise TypeError("Data is corrupted")
 
+class FileData:
+    def __init__(self, file_pah : str, block_size : int = Config.BLOCKSIZE) -> None:
+        pass
 
+    def __iter__(self):
+        return FileDataIterator(self)
+
+class FileDataIterator:
+    def __init__(self, file_data : FileData) -> None:
+        pass
+
+    def __next__(self):
+        pass
+
+# Must give a close function
 class Sender:
     def __init__(self) -> None:
         pass
+    '''
+    while nextpacket.is_last_one():
+        if command == "next":
+            nextpacket = next(packetiterator)
+        send_packet(nextpacket)
+        commad = recive_command()
+    '''
+    
 
 class Reciver:
     def __init__(self) -> None:
         pass
+    
+    '''
+    Ricevi il paccetto
+    se giusto invia la richiesta del prossimo
+    se sbagliato richiedilo
+    
+    '''
