14d577a04be4f5725d0251392fd30967ba215736
Filippo Di Pietro <filippodp9845@gmail.com>
2022-07-26 14:05:48 +0200

    Update file_transfer.py
    Added DataFile class

--- src/utils/file_transfer.py
+++ src/utils/file_transfer.py
@@ -1,7 +1,31 @@
+from hashlib import md5
+from json import dumps
+from typing_extensions import Self
+
+class DataFile:
+    def __init__(self, data : bytes) -> None:
+        self.data = data
+        self.hash = md5(data).hexdigest()
+    
+    def to_json(self) -> str:
+        return dumps({"data" : self.data.decode(), "hash" : self.hash})
+    
+    def to_byte(self) -> bytes:
+        return self.to_json().encode()
+    
+    @classmethod
+    def from_data(cls, data : bytes, digest : str) -> Self:
+        packet = cls(data)
+        if packet.hash == digest:
+            return packet
+        else:
+            raise TypeError("Data is corrupted")
+
 
 class Sender:
     def __init__(self) -> None:
         pass
 
 class Reciver:
-    pass
+    def __init__(self) -> None:
+        pass
