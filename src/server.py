0bfb193ebdd5d2ccc40cdedc54c5b43de2acd37b
Filippo Di Pietro <filippodp9845@gmail.com>
2022-07-27 21:51:55 +0200

    Update server.py
    well defined the parth of the server for listing files

--- src/server.py
+++ src/server.py
@@ -27,7 +27,7 @@ class Server(PacketTransmitter):
             self.commands[command]()
 
     def list_files(self) -> int:
-        files = scandir(path=Config.SERVER_DIR)
+        files = scandir(path = "../" + Config.SERVER_DIR)
         real_file = list(filter(lambda x: x.is_file, files))
         real_file = dumps([file.name for file in real_file])
         self._send_packet(Packet(real_file))
