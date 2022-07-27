a305038b57ddeaa87f0da4894144003b5bd51bd8
Filippo Di Pietro <filippodp9845@gmail.com>
2022-07-27 19:09:35 +0200

    Update client.py
    mockup of client

--- src/client.py
+++ src/client.py
@@ -1,7 +1,28 @@
+from re import S
 from utils.config import Config
+from utils.file_transfer import PacketTransmitter, Packet
+import socket as sk
 
-class Client:
-    pass
+class Client(PacketTransmitter):
+    def __init__(self, address : tuple=Config.ADDRESS, timeout : float=Config.TIMEOUT) -> None:
+        self.socket = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
+        self.socket.timeout(timeout)
+        super().__init__(self.socket, address, Config.BUFFERSIZE)
+    
+    def recive_commands(self) -> None:
+        pass
+
+    def send_command(self) -> None:
+        pass
+
+    def upload_file(self) -> int:
+        pass
+
+    def download_file(self) -> int:
+        pass
+
+    def close(self):
+        self.socket.close()
 
 if __name__ == "__main__":
-    pass
+    client = Client()
